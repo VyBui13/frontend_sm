da7159e4f3c1626ff946c91362ec81f01ec5fd24
theworstkhoi <kylekhoitv@gmail.com>
2025-04-25 16:09:53 +0700

    chore: eslint and prettier format configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: [
+      "next",
+      "next/core-web-vitals",
+      "plugin:tailwindcss/recommended",
+      "plugin:prettier/recommended",
+    ],
+    plugins: ["tailwindcss"],
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
